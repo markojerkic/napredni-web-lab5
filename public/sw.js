0e7d2a446d3c39ec78df998ab10d20bae2ef4c56
Marko Jekrić <markojerkic266@gmail.com>
2023-12-16 16:49:55 +0100

    move static to public

---
+++ public/sw.js
@@ -0,0 +1,74 @@
+const staticCacheName = "static-cache-v2";
+
+const filesToCache = [
+    "/",
+    "manifest.json",
+    "index.html",
+    "offline.html",
+    "404.html",
+];
+
+
+self.addEventListener("install", (event) => {
+    console.log("Attempting to install service worker and cache static assets");
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+elf.addEventListener("activate", (event) => {
+    console.log("**************************************");
+    console.log("**   Activating new service worker... **");
+    console.log("**************************************");
+    const cacheWhitelist = [staticCacheName];
+    // Ovako možemo obrisati sve ostale cacheve koji nisu naš
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(async (response) => {
+                if (response) {
+                    console.log("Found " + event.request.url + " in cache!");
+                    return response;
+                }
+                console.log("----------------->> Network request for ",
+                    event.request.url
+                );
+                return fetch(event.request).then((response) => {
+                    // console.log("response.status = " + response.status);
+                    if (response.status === 404) {
+                        return caches.match("404.html");
+                    }
+                    return caches.open(staticCacheName).then((cache) => {
+                        // console.log(">>> Caching: " + event.request.url);
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("Error", event.request.url, error);
+                // ovdje možemo pregledati header od zahtjeva i možda vratiti različite fallback sadržaje
+                // za različite zahtjeve - npr. ako je zahtjev za slikom možemo vratiti fallback sliku iz cachea
+                // ali zasad, za sve vraćamo samo offline.html:
+                return caches.match("offline.html");
+            })
+    );
+});
+
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import path from "path"
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/sw.js", async (req, res) => {
+    const index = Bun.file("./sw.js");
+    res.set("content-type", "text/js");
+
+    res.type("js");
+    res.send(await index.text());
+});
+
 app.get("/", async (req, res) => {
-
-    const index = Bun.file("./index.html");
-
+    const index = Bun.file("./public/index.html");
     res.send(await index.text());
 });
 
